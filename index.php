7facc98086eb609022ecc220dd303d22179948d2
TolMen <cotton.frachisse@gmail.com>
2025-02-22 02:17:21 +0100

    Adding index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+- Démarre une session
+- Start a session
+*/
+session_start();
+
+/*
+- Inclusion de fichier nécessaire
+- Necessary file inclusion
+*/
+require_once 'src/controller/BDDController/connectBDD.php';
+
+/*
+- Vérifie les paramètres après ? dans l'URL, si vide redirection vers la page de connexion
+- Check the settings afterwards? in URL, if empty redirect to login page
+*/
+if (empty($_SERVER['QUERY_STRING'])) {
+    header("Location: src/views/form/loginForm");
+    throw new Exception("Redirection vers la page de connexion.");
+}
