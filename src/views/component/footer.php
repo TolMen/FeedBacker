b5f6622e65e2a0047f2c7981792fa5b54885c86c
TolMen Jy <cotton.frachisse@gmail.com>
2025-02-26 13:15:51 +0100

    Complete css style overhaul

---
+++ src/views/component/footer.php
@@ -0,0 +1,13 @@
+<footer style="background-color: #181118; border-top: 2px solid #5E3061;" class="text-light py-4">
+    <div class="container text-center">
+        <p class="mb-0">
+            &copy;
+            <!-- Affiche l'année en cours -->
+            <?php
+            date_default_timezone_set("Europe/Paris");
+            echo date("Y");
+            ?>
+            TolMen JY | Tous les droits sont réservés.
+        </p>
+    </div>
+</footer>
