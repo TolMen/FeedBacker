db1b655d09fa0d1257b45c72234f48a926a44ccd
TolMen Jy <cotton.frachisse@gmail.com>
2025-02-27 10:43:40 +0100

    Adding a central page

---
+++ src/views/page/home.php
@@ -0,0 +1,25 @@
+<?php
+session_name("main");
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <!-- Inclusion of meta tags -->
+    <?php include '../component/head.php'; ?>
+
+    <title>Accueil</title>
+</head>
+
+<body>
+
+    <!-- Including the navigation bar -->
+    <?php include '../component/navbar.php' ?>
+
+    <!-- Footer inclusion -->
+    <?php include '../component/footer.php' ?>
+</body>
+
+</html>
