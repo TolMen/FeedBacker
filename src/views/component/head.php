f078b0ba16c13df0c95da239bab8ffd637c3a648
TolMen Jy <cotton.frachisse@gmail.com>
2025-02-27 15:53:14 +0100

    Fix minor

--- src/views/component/head.php
+++ src/views/component/head.php
@@ -18,7 +18,7 @@
     <!-- - External style sheets -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous" />
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 
     <!-- Custom style sheets -->
     <link rel="stylesheet" href="../../../public/css/baseStyle.css" />
