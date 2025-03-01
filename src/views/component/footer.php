5713ce884ed80873ce6a0c78613ea69335210fc8
TolMen Jy <cotton.frachisse@gmail.com>
2025-03-01 18:15:42 +0100

    Fix minor

--- src/views/component/footer.php
+++ src/views/component/footer.php
@@ -1,8 +1,7 @@
-<footer style="background-color: #181118; border-top: 2px solid #5E3061;" class="text-light py-4">
+<footer class="bg-dark text-light py-4">
     <div class="container text-center">
         <p class="mb-0">
             &copy;
-            <!-- Affiche l'année en cours -->
             <?php
             date_default_timezone_set("Europe/Paris");
             echo date("Y");
--- src/views/component/navbar.php
+++ src/views/component/navbar.php
@@ -2,21 +2,20 @@
 $current_page = basename($_SERVER['PHP_SELF']);
 ?>
 
-<nav class="navbar navbar-expand-lg" style="background-color: #181118; border-bottom: 2px solid #5E3061;">
+<nav class="navbar navbar-expand-lg navbar-light bg-light" style="border-bottom: 1px solid black;">
     <div class="container-fluid">
-        <span class="navbar-brand text-light">FeedBacker</span>
+        <span class="navbar-brand">FeedBacker</span>
         <?php if (!empty($_SESSION['id'])) { ?>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
                 <ul class="navbar-nav mb-2 mb-lg-0">
-                    <a class="nav-link" href="home.php" style="color: <?php echo ($current_page == 'home.php') ? '#E796F3' : 'white'; ?>;">Accueil</a>
                     <li class="nav-item">
-                        <a class="nav-link" style="color: #FF9592;" href="../../controller/userController/userStop.php" title="Déconnexion">Déconnexion</a>
+                        <a class="nav-link text-danger" href="../../controller/userController/userStop.php" title="Déconnexion">Déconnexion</a>
                     </li>
-                <?php } ?>
                 </ul>
             </div>
+        <?php } ?>
     </div>
 </nav>
