d8f95cb93449ac1f42677bde50ace8d248f27079
TolMen Jy <cotton.frachisse@gmail.com>
2025-03-02 13:13:37 +0100

    Model and controller logic to add a student to the database & to a class

---
+++ src/controller/classController/classAddStudent.php
@@ -0,0 +1,55 @@
+<?php
+
+/* 
+- Reprend une session existante
+- Resumes an existing session
+*/
+session_name("main");
+session_start();
+
+/* 
+- Inclusion des fichiers nécessaire
+- Inclusion of necessary files
+*/
+require_once '../../model/userModel/userAddModel.php';
+
+if (isset($_POST['add_student'])) {
+
+    /*
+    - Sécurisation des données
+    - Data security
+    */
+    $first_name = htmlspecialchars(trim($_POST['first_name']));
+    $name = htmlspecialchars(trim($_POST['name']));
+    $class_id = $_POST['class_id'];
+
+    /*
+    - Génération automatique du pseudo : 1ère lettre du prénom + nom (en minuscule)
+    - Automatic pseudo generation: first letter of first name + last name (all lowercase)
+    */
+    $pseudo = strtolower(substr($first_name, 0, 1) . $name);
+
+
+    /*
+    - Enregistrement de l'élève dans la base de données avec ses informations
+    - Register the student in the database with their information
+    */
+    $studentAddModel = new UserAddModel();
+    if ($studentAddModel->insertStudent($bdd, $first_name, $name, $pseudo, $class_id)) {
+
+        /*
+        - Gestion des logs par un message et un appel de fonction
+        - Logs management by a message and a function call
+        */
+        $logWrite = new LogWriteModel();
+        $message = "Ajout réussie pour l'élève au pseudo '$pseudo' - " . date("d-m-Y H:i:s") . PHP_EOL . PHP_EOL;
+        $logWrite->writeLog($message, "../../../logFiles/addStudent.log");
+
+        /*
+        - Redirection vers le tableau de bord
+        - Redirect to dashboard
+        */
+        header('Location: ../../views/page/dashboard.php');
+        throw new Exception("Redirection vers le tableau de bord");
+    }
+}
---
+++ src/model/userModel/userAddModel.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+- Inclusion de fichier nécessaire
+- Necessary file inclusion
+*/
+require_once '../../controller/BDDController/connectBDD.php';
+
+/*
+- Classe pour gérer les opérations d'inscription
+- Class to manage the registration operations
+*/
+class UserAddModel
+{
+
+    /*
+    - Cette fonction insère les informations utilisateurs
+    - This function inserts user information
+    */
+    public function insertStudent(PDO $bdd, $first_name, $name, $pseudo, $class_id)
+    {
+        $role = 'eleve';
+        $passwordOne = hash('sha256', $pseudo);
+        $insertUser = $bdd->prepare('INSERT INTO user(first_name, name, password, pseudo, role, class_id) VALUES (?, ?, ?, ?, ?, ?)');
+        $success = $insertUser->execute([$first_name, $name, $passwordOne, $pseudo, $role, $class_id]);
+
+        if ($success && $class_id) {
+            $updateNbStudent = $bdd->prepare('UPDATE class SET nb_student = nb_student + 1 WHERE id = ? AND nb_student > 0');
+            $updateNbStudent->execute([$class_id]);
+        }
+
+        return $success;
+    }
+}
